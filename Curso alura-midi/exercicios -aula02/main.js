eb0b96e615191315e4d99bd54e00eba3e61d502a
vitoria.sousa <maria.v.cosmo@gmail.com>
2024-05-17 13:25:55 -0300

    exercicio 01 da aula02

---
+++ Curso alura-midi/exercicios -aula02/main.js
@@ -0,0 +1,31 @@
+function tocaPom() {
+    document.querySelector('#som_tecla_pom').play();
+}
+function tocaClap() {
+    document.querySelector('#som_tecla_clap').play();
+}
+function tocaTim() {
+    document.querySelector('#som_tecla_tim').play();
+}
+
+
+function tocaPuff() {
+    document.querySelector('#som_tecla_puff').play()
+}
+function tocaSplash() {
+    document.querySelector('#som_tecla_splash').play();
+}
+function tocaToim() {
+    document.querySelector('#som_tecla_toim').play();
+}
+
+
+function tocaPsh() {
+    document.querySelector('#som_tecla_psh').play();
+}
+function tocaTic() {
+    document.querySelector('#som_tecla_tic').play();
+}
+function tocaTom() {
+    document.querySelector('#som_tecla_tom').play();
+}
