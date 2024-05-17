2085213ce4ce2d503431388cd922aefdfce583cb
vitoria.sousa <maria.v.cosmo@gmail.com>
2024-05-17 13:42:33 -0300

    exercicio 02 da aula01

---
+++ Curso alura-midi/exercicios -aula02/ex02/main.js
@@ -0,0 +1,44 @@
+function tocaPom() {
+    document.querySelector('#som_tecla_pom').play();
+}
+function tocaClap() {
+    document.querySelector('#som_tecla_clap').play();
+}
+function tocaTim() {
+    document.querySelector('#som_tecla_tim').play();
+}
+
+
+function tocaPuff() {
+    document.querySelector('#som_tecla_puff').play()
+}
+function tocaSplash() {
+    document.querySelector('#som_tecla_splash').play();
+}
+function tocaToim() {
+    document.querySelector('#som_tecla_toim').play();
+}
+
+
+function tocaPsh() {
+    document.querySelector('#som_tecla_psh').play();
+}
+function tocaTic() {
+    document.querySelector('#som_tecla_tic').play();
+}
+function tocaTom() {
+    document.querySelector('#som_tecla_tom').play();
+}
+
+
+document.querySelector('.tecla_pom').onclick = tocaPom;
+document.querySelector('.tecla_clap').onclick = tocaClap;
+document.querySelector('.tecla_tim').onclick = tocaTim;
+
+document.querySelector('.tecla_puff').onclick = tocaPuff;
+document.querySelector('.tecla_splash').onclick = tocaSplash;
+document.querySelector('.tecla_toim').onclick = tocaToim;
+
+document.querySelector('.tecla_psh').onclick = tocaPsh;
+document.querySelector('.tecla_tic').onclick = tocaTic
+document.querySelector('.tecla_tom').onclick = tocaTom;
