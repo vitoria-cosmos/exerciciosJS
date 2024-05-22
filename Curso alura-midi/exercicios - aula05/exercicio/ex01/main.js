db72e0396d35bf697a3beb90a9a5efcffe52c972
vitoria.sousa <maria.v.cosmo@gmail.com>
2024-05-22 20:00:10 -0300

    resolvendo problemas com os arquivos

--- Curso alura-midi/exercicios - aula05/exercicio/ex01/main.js
+++ Curso alura-midi/exercicios - aula05/exercicio/ex01/main.js
@@ -3,12 +3,8 @@ const teclas = document.querySelectorAll('.tecla');
 for (let contador = 0; contador < teclas.length; contador++) {
     const tecla = teclas[contador];
     tecla.onclick = function() {
-        if (tecla.classList[tecla.classList.length - 1] === 'ativa') {
-            tecla.classList.remove('ativa');
-        } else {
-            tecla.classList.add('ativa');
-        }
-    }   
+        tecla.classList.add('ativa');
+    }
 }
 
 
