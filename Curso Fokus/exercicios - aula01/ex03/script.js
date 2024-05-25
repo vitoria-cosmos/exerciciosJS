9bb5d21e8e4fd68751bc8d62a61990130ab8e6df
vitoria.sousa <maria.v.cosmo@gmail.com>
2024-05-25 18:54:29 -0300

    ex03 finalizado

--- Curso Fokus/exercicios - aula01/ex03/script.js
+++ Curso Fokus/exercicios - aula01/ex03/script.js
@@ -12,3 +12,7 @@ const botao = document.querySelectorById('start-pause');
 const btFoco = document.querySelector('.app__card-button--foco');
 const btCurto = document.querySelector('.app__card-button--curto');
 const btLongo = document.querySelector('.app__card-button--longo');
+
+const tempFoco = 1500;
+const tempCurto = 300;
+const tempLongo = 900;
