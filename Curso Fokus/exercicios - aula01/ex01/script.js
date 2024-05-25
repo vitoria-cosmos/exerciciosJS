ea792f7f62a2e03e0f8aa43c5037a69ca14dfd47
vitoria.sousa <maria.v.cosmo@gmail.com>
2024-05-25 15:05:25 -0300

    ex01

--- Curso Fokus/exercicios - aula01/ex01/script.js
+++ Curso Fokus/exercicios - aula01/ex01/script.js
@@ -1,17 +1,4 @@
-const html = document.querySelector('html');
-const focoBt = document.querySelector('.app__card-button--foco');
-const curtoBt = document.querySelector('.app__card-button--curto');
-const longoBt = document.querySelector('.app__card-button--longo');
+document.querySelector('html');
 
-focoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'foco');
-})
-
-curtoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-curto');
-})
-
-longoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-longo');
-})
+document.querySelectorById('#timer');
 
