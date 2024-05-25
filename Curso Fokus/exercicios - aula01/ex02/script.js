9ba6b1da0c40434a929d7dfb980422776adc2236
vitoria.sousa <maria.v.cosmo@gmail.com>
2024-05-25 18:12:21 -0300

    ex02 finalizado

---
+++ Curso Fokus/exercicios - aula01/ex02/script.js
@@ -0,0 +1,7 @@
+const html = document.querySelector('html');
+
+const temporaliazador = document.querySelectorById('#timer');
+
+const imagem = document.querySelector('.app__image');
+
+const texto = document.querySelector('.app__title');
