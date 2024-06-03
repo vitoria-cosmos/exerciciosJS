89e0c1140625a6229152842dfb2b442e62de1dc1
vitoria.sousa <maria.v.cosmo@gmail.com>
2024-06-03 09:51:17 -0300

    adicionando arquivos e organizando pastas

---
+++ Curso Fokus/exercicios - aula02/ex01/script.js
@@ -0,0 +1,11 @@
+const html = document.querySelector('html');
+
+const imagem = document.querySelector('.app__image');
+
+const texto = document.querySelector('.app__title');
+
+const btFoco = document.querySelector('.app__card-button--foco');
+const btCurto = document.querySelector('.app__card-button--curto');
+const btLongo = document.querySelector('.app__card-button--longo');
+
+
