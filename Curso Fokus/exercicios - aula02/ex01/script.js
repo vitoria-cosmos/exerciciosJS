61d3b6123ce9b30b8b172d47c1652fc28d4dde62
vitoria.sousa <maria.v.cosmo@gmail.com>
2024-06-03 10:17:13 -0300

    exercicios da aula02 finalizados

--- Curso Fokus/exercicios - aula02/ex01/script.js
+++ Curso Fokus/exercicios - aula02/ex01/script.js
@@ -9,3 +9,30 @@ const btCurto = document.querySelector('.app__card-button--curto');
 const btLongo = document.querySelector('.app__card-button--longo');
 
 
+function alterarContexto(contexto) {
+    html.setAttribute('data-contexto', contexto);
+    imagem.setAttribute('scr', `./imagens/${contexto}.png`);
+}
+
+btFoco.addEventListener('click', function() {
+    alterarContexto('foco');
+texto.innerHTML = 
+
+`Otimize sua produtividade,<br>
+<strong class="app__title-strong">mergulhe no que importa.</strong>`;
+
+});
+
+btCurto.addEventListener('click', function() {
+    alterarContexto('descanso-curto');
+    texto.innerHTML = 
+    `Que tal dar uma respirada? <br>
+    <strong class='app__title-strong'> Faça uma pausa curta!</strong>`
+});
+
+btLongo.addEventListener('click', function() {
+    alterarContexto('descanso-longo');
+    texto.innerHTML = 
+    `Hora de voltar à superfície. <br>
+    <strong class='app__title-strong'> Faça uma pausa longa.</strong>`;
+});
