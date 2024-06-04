b1e4444326c7cc897b3bcbe2337507d6ab067c85
vitoria.sousa <maria.v.cosmo@gmail.com>
2024-06-04 11:46:07 -0300

    adicionando novos arquivos e exercício 01 da aula03 finalizado

--- Curso Fokus/exercicios - aula03/ex01/main.js
+++ Curso Fokus/exercicios - aula03/ex01/main.js
@@ -4,17 +4,23 @@ const btCurto = document.querySelector('.descanso-curto');
 const btLongo = document.querySelector('.descanso-longo');
 
 const botoes = document.querySelectorAll('.buttons');
+console.log(botoes);
 
 btFoco.addEventListener('click', () => {
-    btFoco.classList.add('active');
-
+    mudaContexto(btFoco);
 });
 
 btCurto.addEventListener('click', () => {
-    btCurto.classList.add('active');
+    mudaContexto(btCurto);
 });
 
 btLongo.addEventListener('click', () => {
-    btLongo.classList.add('active');
+    mudaContexto(btLongo);
 });
 
+function mudaContexto(contexto) {
+    botoes.forEach(function (botao) {
+        botao.classList.remove('active');
+    });
+    contexto.classList.add('active');    
+};
